4eb014103a79fb9be29054f4d7a74393ed73f9ce
Lyshak S <sasha.lishak@gmail.com>
2021-01-27 16:05:15 +0200

    added info

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function zeros(str, lng) {
-    var factlength = str.length;
+    let factlength = str.length;
     if (factlength < lng) {
         for (var i = 0; i < (lng - factlength); i++) {
             str = '0' + str;
@@ -8,17 +8,34 @@ function zeros(str, lng) {
     return str;
 }
 
-function generate() {
-    var birth = zeros(String(Math.floor((Math.random() * 16601)) + 20000), 5);
-    var poryadk = zeros(String(Math.floor((Math.random() * 9000))), 4);
-    var rezult = birth + poryadk;
-    var kontr = String(((
-        (-1) * rezult[0] + 5 * rezult[1] + 7 * rezult[2] +
-        9 * rezult[3] + 4 * rezult[4] + 6 * rezult[5] +
-        10 * rezult[6] + 5 * rezult[7] + 7 * rezult[8]
-    ) % 11) % 10);
-    rezult = rezult + kontr;
-
-    document.getElementById('code').innerText = rezult;
-
+function calculateAge(birthday) {
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
+
+function generate() {
+    let birth = zeros(String(Math.floor((Math.random() * 16601)) + 20000), 5);
+    let poryadk = zeros(String(Math.floor((Math.random() * 9000))), 4);
+    let result = birth + poryadk;
+    let kontr = String(((
+        (-1) * result[0] + 5 * result[1] + 7 * result[2] +
+        9 * result[3] + 4 * result[4] + 6 * result[5] +
+        10 * result[6] + 5 * result[7] + 7 * result[8]
+    ) % 11) % 10);
+    result = result + kontr;
+
+    const gender = result[8] % 2 ? 'Man' : 'Woman';
+
+    const startDate = new Date('1899-12-31').getTime();
+    const addTime = Math.floor(result / 100000) * 86400000;
+    const birthday = new Date(startDate + addTime);
+
+    document.getElementById('code').innerText = result;
+    document.getElementById('gender').innerText = gender;
+    document.getElementById('age').innerText = calculateAge(birthday);
+    document.getElementById('birthday').innerText = birthday.toDateString();
+}
+
+
+generate();
